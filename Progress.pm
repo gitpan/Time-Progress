87324cee2bd8ef931d8cc198b3dfc17832170567
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2007-11-29 22:00:14 +0000

    Import of CADE/Time-Progress-1.3 from CPAN.
    gitpan-cpan-distribution: Time-Progress
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         CADE/Time-Progress-1.3.tar.gz
    gitpan-cpan-author:       CADE
    gitpan-cpan-maturity:     released

--- Progress.pm
+++ Progress.pm
@@ -2,7 +2,7 @@ package Time::Progress;
 use Exporter;
 our @ISA = qw( Exporter );
 our @EXPORT = qw(  );
-our $VERSION = '1.2';
+our $VERSION = '1.3';
 use strict;
 use warnings;
 use Carp;
@@ -41,8 +41,10 @@ sub attr
 sub restart
 {
   my $self = shift;
+  my @ret = $self->attr( @_ );
   $self->{ 'start' } = time();
   $self->{ 'stop'  } = undef;
+  return @ret;
 }
 
 sub stop
@@ -151,22 +153,23 @@ Time::Progress - Elapsed and estimated finish time reporting.
 
 =head1 SYNOPSIS
 
+  use Time::Progress;
   # autoflush to get \r working
   $| = 1;
   # get new `timer'
   my $p = new Time::Progress;
- 
+
   # restart and report progress
   $p->restart;
   sleep 5; # or do some work here
   print $p->report( "done %p elapsed: %L (%l sec), ETA %E (%e sec)\n", 50 );
 
   # set min and max values
-  $p->attr( min => -2, max => -20 );
+  $p->attr( min => 2, max => 20 );
   # restart `timer'
   $p->restart;
   my $c;
-  for( $c = -2; $c >= -20; $c-- )
+  for( $c = 2; $c <= 20; $c++ )
     {
     # print progress bar and percentage done
     print $p->report( "eta: %E min, %40b %p\r", $c );
@@ -174,14 +177,13 @@ Time::Progress - Elapsed and estimated finish time reporting.
     }
   # stop timer
   $p->stop;
- 
+
   # report times
   print $p->elapsed_str;
 
 =head1 DESCRIPTION
 
-Shortest time interval that can be measured is 1 second. (This should be
-fixed in the future perhaps). The available methods are:
+Shortest time interval that can be measured is 1 second. The available methods are:
 
 =over 4
 
@@ -195,7 +197,20 @@ default to percents range.
 
 =item restart
 
-restarts the timer and clears the stop mark.
+restarts the timer and clears the stop mark. optionally restart() may act also
+as attr() for setting attributes:
+
+  $p->restart( min => 1, max => 5 );
+
+is the same as:
+
+  $p->attr( min => 1, max => 5 );
+  $p->restart();
+
+If you need to count things, you can set just 'max' attribute since 'min' is
+already set to 0 when object is constructed by new():
+
+  $p->restart( max => 42 );
 
 =item stop
 
@@ -209,13 +224,13 @@ like:
   # do some post-work here
   print $p->report;
   # `post-work' will not be timed
- 
+
 Stop is useless if you want to report time as soon as work is finished like:
 
   $p->restart;
   # do some work here...
   print $p->report;
- 
+
 =item continue
 
 Clears the stop mark. (mostly useless, perhaps you need to B<restart>?)
@@ -246,7 +261,7 @@ without parameters.
 B<attr> returns array of the set attributes:
 
   my ( $new_min, $new_max ) = $p->attr( min => 1, max => 5 );
- 
+
 If you want just to get values use undef:
 
   my $old_format = $p->attr( format => undef );
@@ -305,7 +320,7 @@ progress bar which looks like:
   %40b -- 40-chars wide bar
   %9b  --  9-chars wide bar
   %b   -- 79-chars wide bar (default)
- 
+
 =back
 
 Parameters can be ommited and then default format set with B<attr> will
@@ -341,15 +356,15 @@ helpers -- return elapsed/estimated seconds or string in format:
 
   # $c is current element (step) reached
   # for the examples: min = 0, max = 100, $c = 33.3
- 
+
   print $p->report( "done %p elapsed: %L (%l sec), ETA %E (%e sec)\n", $c );
   # prints:
   # done  33.3% elapsed time   0:05 (5 sec), ETA   0:07 (7 sec)
- 
+
   print $p->report( "%45b %p\r", $c );
   # prints:
   # ###############..............................  33.3%
- 
+
   print $p->report( "done %p ETA %f\n", $c );
   # prints:
   # done  33.3% ETA Sun Oct 21 16:50:57 2001
@@ -360,10 +375,10 @@ helpers -- return elapsed/estimated seconds or string in format:
 
   <cade@biscom.net> <cade@datamax.bg> <cade@cpan.org>
 
-  http://www.biscom.net/~cade
- 
+  http://cade.datamax.bg
+
 =head1 VERSION
 
-  $Id: Progress.pm,v 1.2 2002/12/16 08:29:22 cade Exp $
- 
+  $Id: Progress.pm,v 1.3 2005/08/25 21:24:39 cade Exp $
+
 =cut
